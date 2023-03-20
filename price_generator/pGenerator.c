c353b30e698418c3ee746083679d3892e455df08
zablon <zablonkiprotich16@gmail.com>
2023-03-20 20:28:48 +0000

    Edit solution for pGenerator

--- price_generator/pGenerator.c
+++ price_generator/pGenerator.c
@@ -11,16 +11,33 @@ int main(void)
 	int price;
 	int ml;
 	int i;
+	int sPrice;
+	char choice;
+	double mlPrice;
+	double priceMl;
 
 	ml = 1000;
+	printf("Hey, what can i do for you today? choose A or B\n");
+	printf("A. Print all prices with Millimeters equivalent\n");
+	printf("B. Print how many millimeter is equivalent to entered amount\n:");
+	scanf("%c", &choice);
 	printf("Enter price per litre: ");
 	scanf("%d", &price);
-	for (i = 100; i <= ml; i += 50)
-	{
-		double mlPrice;
 
-		mlPrice = i * price / ml;
-		printf("%d ML => %5.2f/= \n", i, mlPrice);
+	if (choice == 'A')
+	{
+		for (i = 100; i <= ml; i += 50)
+		{
+			mlPrice = i * price / ml;
+			printf("%d ML => %5.2f/= \n", i, mlPrice);
+		}
+	}
+	else if (choice == 'B')
+	{
+		printf("Now enter the price to be converted: ");
+		scanf("%d", &sPrice);
+		priceMl = sPrice * ml / price;
+		printf("\nKsh %d is equivalent to %5.0f ML\n", sPrice, priceMl);
 	}
 
 	return (0);
